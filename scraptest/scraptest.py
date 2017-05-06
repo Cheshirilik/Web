beb81c42306cd62f34622129b7d84f5a1d8740c4
Cheshirilik <avfriend@mail.ru>
2017-05-06 16:02:00 +0300

    get_tag added

--- scraptest/scraptest.py
+++ scraptest/scraptest.py
@@ -1,1 +1,32 @@
-print("hello")
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from bs4 import BeautifulSoup
+
+
+def get_tag(url, tag=None):
+    try:
+        html = urlopen(url)
+
+    except HTTPError:                                # 404 или похожее
+        return None
+
+    try:
+        soup = BeautifulSoup(html.read())
+        obj = soup.html.head
+    except AttributeError:                           # обращение к атрибуту отсутствующего тэга
+        return None
+    else:
+        if obj:
+            return obj
+        else:                                        # не найден тэг
+            return None
+
+
+obj = get_tag("http://pythonscraping.com/pages/page1.html")
+if obj:
+    print(obj)
+else:  # не найден тэг
+    print("Tag not found!")
+
+
+
