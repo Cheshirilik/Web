fe1f766dc613838f2dd445af65ab7ef2c90170ec
Cheshirilik <avfriend@mail.ru>
2017-05-15 16:43:29 +0300

    Добавлены пробные ф-ции get_wiki_links1 и 2

--- scraptest/crawler.py
+++ scraptest/crawler.py
@@ -7,9 +7,10 @@ import re
 
 random.seed(datetime.datetime.now())
 
+
 def get_soup(url):
     try:
-        html = urlopen("http://wikipedia.org" + url)
+        html = urlopen("http://en.wikipedia.org" + url)
     except HTTPError:                                # 404 или похожее
         return None
 
@@ -19,13 +20,53 @@ def get_soup(url):
 def get_wiki_links(url):
     soup = get_soup(url)
 
-    return soup.find("div", {"id":"bodyContent"}).findAll("a", href=re.compile("^(/wiki/)((?!:).)*$"))
+    return soup.find("div", {"id": "bodyContent"}).findAll("a", href=re.compile("^(/wiki/)((?!:).)*$"))
+
+
+pages = set()
+def get_wiki_links1(url):
+    global pages
+
+    soup = get_soup(url)
+    for i in soup.findAll("a", href=re.compile("^(/wiki/)")):
+        if "href" in i.attrs:
+            if i.attrs['href'] not in pages:
+                new = i.attrs['href']
+                print(new)
+                pages.add(new)
+                get_wiki_links1(new)
+
+
+def get_wiki_links2(url):
+    global pages
+
+    soup = get_soup(url)
+    try:
+        print("h1.get_text = " + soup.h1.get_text())
+        print("mw-content-text = ")
+        print(soup.find(id="mw-content-text").findAll("p")[0])
+    except AttributeError:
+        print("Something bad happened")
+
+    for i in soup.findAll("a", href=re.compile("^(/wiki/)")):
+        if "href" in i.attrs:
+            if i.attrs['href'] not in pages:
+                new = i.attrs['href']
+                print("------\n" + new)
+                pages.add(new)
+                get_wiki_links2(new)
 
 
 #======== MAIN ========
-links = get_wiki_links("/wiki/Kevin_Bacon")
-print(links)
-while len(links) > 0:
-    article = links[random.randint(0, len(links)-1)].attrs["href"]
-    print(article)
-    links = get_wiki_links(article)
+# links = get_wiki_links("/wiki/Kevin_Bacon")
+# print(links)
+# while len(links) > 0:
+#     article = links[random.randint(0, len(links)-1)].attrs["href"]
+#     print(article)
+#     links = get_wiki_links(article)
+
+# get_wiki_links1("")
+# get_wiki_links1("/wiki/Kevin_Bacon")
+
+get_wiki_links2("")
+
